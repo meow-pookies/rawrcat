b2c2574736eb36c4a87877e294311ff067aa2699
pookie <117560921+sapphxyre@users.noreply.github.com>
2026-03-02 12:06:52 -0500

    Cretate file - fsutil.py

---
+++ lib/fsutil.py
@@ -0,0 +1,31 @@
+# rawrcat v1.0
+# 'fsutil.py' -> file system management utility -> v1.0
+# application resource file control management
+# python version 3.13.5
+# dependencies: 'os'
+# [!] undefined exception handling rules
+
+# import dependencies
+import os, uuid
+
+# import local packages
+from ..lib import pkcs1_oaep as cryptography
+from ..lib.iomgr import iostream as xcnsl
+
+# object 'init' { (takes no parameters) -> null }
+class init:
+
+    # attempt to locate *.pem keyfiles
+    def keychain():
+        if cryptography.keyman.scandir(os.getcwd()) == False:
+            xcnsl.log('no keys were found in the current directory or its subdirectories\n ..\\  ' + str(os.getcwd()))
+            keypath = xcnsl.update('set key file target directory or press <enter> to create a new rsa key pair')
+            if keypath == True:
+                xcnsl.log('', 5, 'creating new keys:')
+                publickey_path = xcnsl.input('set public key target')
+                privatekey_path = xcnsl.input('set private key target')
+                cryptography.keyman.newkey(publickey_path, privatekey_path, uuid.uuid4().hex, uuid.uuid4().hex)
+            else:
+                if os.path.exists(keypath):
+                    if cryptography.keyman.scandir(keypath) == False:
+                        pass
--- main.py
+++ main.py
@@ -26,17 +26,3 @@ xcnsl.log('\n\ninitializing...', 3, "\n\n\n                       _,-'""`-._    
 ostype = str(platform.system().lower())
 xcnsl.log(str('ostype = \'' + str(ostype) + '\''))
 
-# attempt to locate *.pem keyfiles
-if cryptography.keyman.scandir(os.getcwd()) == False:
-    xcnsl.log('no keys were found in the active directory\n ..> ' + str(os.getcwd()))
-    next = xcnsl.update('set key file target directory or press <enter> to create a new rsa key pair')
-    if next == True:
-        xcnsl.log('', 5, 'creating new keys:')
-        publickey_path = xcnsl.input('set public key target')
-        privatekey_path = xcnsl.input('set private key target')
-        cryptography.keyman.newkey(publickey_path, privatekey_path, uuid.uuid4().hex, uuid.uuid4().hex)
-    else:
-        if os.path.exists(next):
-            if cryptography.keyman.scandir(next) == False:
-                pass
-# requires loop
